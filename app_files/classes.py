a977949491edd15a050d95c17d90e96d8bb42dc7
askalburgi <askalburgi@gmail.com>
2016-10-23 02:01:57 -0600

    bugfix

--- app_files/classes.py
+++ app_files/classes.py
@@ -10,12 +10,12 @@ class CareStaff:
     # For a given patient, list all charts in the system ordered by adate (indicating also whether they are closed or open). The user 
     # should be given the option to select a chart. Once a chart is selected, all entries (symptoms, diagnoses, and medications) 
     # associated with that chart must be listed, and the result must be ordered by the date of the entries.
-    def getCharts(patient): 
+    def getCharts(self, patient): 
         print "All Charts"
         pass
     # For a given patient and an open chart of the patient add an entry for symptoms. 
     # The date obs_date should be set to the current date and time.
-    def addSymptom(patient):
+    def addSymptom(self, patient):
         print "Add symptom"
         pass
 
@@ -23,18 +23,18 @@ class Doctor(CareStaff):
     def introduce(self):
         return super(Doctor, self).introduce() + "Doctor"
     
-    def getCharts(patient):
+    def getCharts(self, patient):
         return super(Doctor, self).getCharts(patient)
     
-    def addSymptom(patient):
+    def addSymptom(self, patient):
         return super(Doctor, self).addSymptom(patient)
         
 class Nurse(CareStaff):
     def introduce(self):
         return super(Nurse, self).introduce() + "Nurse"
     
-    def getCharts(patient):
+    def getCharts(self, patient):
         return super(Nurse, self).getCharts(patient)
     
-    def addSymptom(patient):
+    def addSymptom(self, patient):
         return super(Doctor, self).addSymptom(patient)

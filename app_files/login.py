860e1abea8e849adf3931e1780c37f44dba2f9bd
Arjun Kalburgi <askalburgi@gmail.com>
2016-10-28 15:31:26 -0600

    new user flow func

--- app_files/login.py
+++ app_files/login.py
@@ -16,18 +16,25 @@ def decrypt(s):
 		r = r + chr(ord(char) - 2)
 	return r
 
-def start():
-	username = raw_input('Please login with your username: ')
-	password = raw_input('And password: ')
-
-	user = getUser(encrypt(username), encrypt(password)) # return obj of user info, or None if can't be found
-	if user is not None:
+def beginFlow(user): 
 		if user['role'] == 'D':
 			d_flow(user)
 		elif user['role'] == 'N':
 			n_flow(user)
 		elif user['role'] == 'A':
 			a_flow(user)
+
+def start():
+	username = raw_input('Please login with your username: ')
+	password = raw_input('And password: ')
+
+	user = getUser(encrypt(username), encrypt(password)) # return obj of user info, or None if can't be found
+	
+	if user is not None:
+		beginFlow(user)
 	else:
-		print('Invalid username or password! Please try again')
-		start()
+		if raw_input("Not a user, would you like to make a new user with this username and password? (y) ") == "y": 
+			user = createUser(raw_input("Role (D, N or A) "), raw_input("Name "), encrypt(username), encrypt(password))
+			beginFlow(user)
+		else: 
+			start()
